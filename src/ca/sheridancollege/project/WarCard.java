e81567951565dce46b88f9a524786111f6223426
Manjinder Singh <manjindersinghjt@gmail.com>
2023-08-14 16:42:37 -0400

    syn 1

--- src/ca/sheridancollege/project/WarCard.java
+++ src/ca/sheridancollege/project/WarCard.java
@@ -5,11 +5,37 @@ public class WarCard extends Card {
     private Value value;
     private Suit suit;
 
+    //getter for value
+    public Value getValue() {
+        return value;
+    }
+
+    //setter for value
+    public void setValue(Value value) {
+        this.value = value;
+    }
+
+    //getter for suit
+    public Suit getSuit() {
+        return suit;
+    }
+
+    //setter for suit
+    public void setSuit(Suit suit) {
+        this.suit = suit;
+    }
+
     //Constructor
     public WarCard(Value value, Suit suit) {
         this.value = value;
         this.suit = suit;
     }
+
+    //create card
+    public WarCard createCard(){
+        return new WarCard(value, suit);
+    }
+
 
     //Enumaration for Value of cards
     public enum Value {
@@ -20,6 +46,7 @@ public class WarCard extends Card {
     public enum Suit {
         CLUBS, DIAMONDS, HEARTS, SPADES
     };
+
 
     public String toString(){
         return "temp";
