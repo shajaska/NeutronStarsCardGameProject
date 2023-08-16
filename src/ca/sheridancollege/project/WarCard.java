710abba8f76f85847a5b309d5e9ed3973c36873b
Manjinder Singh <manjindersinghjt@gmail.com>
2023-08-15 22:43:07 -0400

    comments in warcard class

--- src/ca/sheridancollege/project/WarCard.java
+++ src/ca/sheridancollege/project/WarCard.java
@@ -8,39 +8,73 @@ package ca.sheridancollege.project;
  * */
 
 public class WarCard extends Card {
-    
+
+    /**
+     * The value of the card
+     * and the suit of the card
+     */
     private Value value;
     private Suit suit;
 
+    /**
+     *Returns the value of the card.
+     * @return the value of the card 
+     */
     public Value getValue() {
         return value;
     }
 
+    /**
+     * Sets the value of the card.
+     * @param value
+     */
     public void setValue(Value value) {
         this.value = value;
     }
 
+    /**
+     * returns the suit of the card.
+     * @return
+     */
     public Suit getSuit() {
         return suit;
     }
 
+    /**
+     * Sets the suit of the card.
+     * @param suit
+     */
     public void setSuit(Suit suit) {
         this.suit = suit;
     }
 
+    /**
+     * Constructor for the WarCard class.
+     * @param value
+     * @param suit
+     */
     public WarCard(Value value, Suit suit) {
         this.value = value;
         this.suit = suit;
     }
 
+    /**
+     * This enum contains the values of the cards.
+     */
     public enum Value {
         TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE
     };
 
+    /**
+     * This enum contains the suits of the cards.
+     */
     public enum Suit {
         CLUBS, DIAMONDS, HEARTS, SPADES
     };
 
+    /**
+     * This method returns the value and suit of the card.
+     */
     @Override
     public String toString() {
         return this.value + " of " + this.suit;
